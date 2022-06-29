24eac50d828db975cdf2fba7f811bf2ee022ab88
bvundurthy <bvundurthy@outlook.com>
2022-06-28 22:12:56 -0500

    Step1a done

--- Step1a_Well_Detection_AutoIdentify_Base.m
+++ Step1a_Well_Detection_AutoIdentify_Base.m
@@ -9,7 +9,6 @@ run('Step0_change_directory.m'); % cd into the condition folder
 run('parameters.m'); % import all necessary parameters for all Steps
 
 %% 1. Reading the image and converting it to binary
-% fbrgt_base = 'dicc1t2xy2.tif'; 
 img_src = imread(fbrgt_base);
 % imshow(img_src);
 disp('Image loading done...');        
@@ -55,7 +54,7 @@ idx = find(wells_comp(:,3)==max(wells_comp(:,3))); % Identify parameters with hi
 max_wells = wells_comp(idx(1),:); % Identify the last parameter with highest wells
 % % Print the parameter with highest wells
 pri = ['i = ', num2str(max_wells(1)), '; j = ',num2str(max_wells(2)), ';num = ',num2str(max_wells(3))]; 
-fprintf(['Maximum number of wells occur with ',pri,'\n']);
+fprintf(['Maximum number of wells occur with ',pri,' (>=90 is excellent).\n']);
 
 %% Morphological calculations and binary conversion using optimal values
 img_morph = max_wells(1)*img_bw - max_wells(2)*imdilate(img_bw, strel('square',5));
@@ -66,7 +65,7 @@ disp('Image conversion to binary done...');
 for i = 1:size(bbl_box(:,1),1)
     img_binary(bbl_box(i,1):bbl_box(i,2), bbl_box(i,3):bbl_box(i,4)) = 0;
     if (i==1)
-        disp ('Air bubbles are being removed from the binary figure...');
+        disp('Air bubbles are present in this condition. Removing them from the binary figure...');
     end
 end
 
@@ -76,8 +75,12 @@ end
 img_edge_log = edge(img_binary, 'log',0);
 img_fill_log = imfill(img_edge_log,'holes');
 img_area_log = bwareafilt(img_fill_log,[18000 30000]); 
-regs = regionprops(img_area_log); % Determining regions
+regs = regionprops(img_area_log,'Area','BoundingBox','Centroid','Image'); % Determining regions
 disp('Wells identification done...'); 
+
+wells_list = [cat(1,regs.Area) cat(1,regs.Centroid) cat(1,regs.BoundingBox)];
+num_wells = size(wells_list,1);
+fprintf('No. of Wells = %d \n',num_wells); 
 
 % % Displays a Figure for quick debugging
 %     figure(iter) 
@@ -93,71 +96,84 @@ disp('Wells identification done...');
 %     subplot(3,2,5)
 %     imshow(img_area_log(6000:9000,6000:9000));
 
-bb = cat(1,regs.Area); wells_num = length(bb);
-fprintf('No. of Wells = %d \n',wells_num); 
-
 %% 4. Separating regions and enumeration
-bb = cat(1,regs.BoundingBox); % Obtaining wells' bounding boxes
+bb = wells_list(:,end-3:end); % Obtaining wells' bounding boxes
 img_size = size(img_area_log);
 img_wells = zeros(img_size(1), img_size(2)); % Creating wells image
 img_bulbs = zeros(img_size(1), img_size(2)); % Creating bulbs image
-num_wells = length(bb(:,1));
-for i = 1:length(bb(:,1))
-    % Identifying bounding box for each well
-    colmin = bb(i,1) - 0.5; 
-    colmax = bb(i,1) + 0.5 + bb(i,3);
-    rowmin = bb(i,2) - 0.5; 
-    rowmax = bb(i,2) + 0.5 + bb(i,4);
-    img_here = img_area_log(rowmin:rowmax, colmin:colmax);
-    
-    % Creating image for wells with the well number
-    img_temp = zeros(bb(i,4)+2, bb(i,3)+2);
-    img_temp(img_here == 1) = i;
-    img_wells(rowmin:rowmax, colmin:colmax) = img_temp;            
+invalid_wells = zeros(num_wells,1); count = 0; 
+for i = 1:num_wells
+    fprintf('Iteration: %d\n',i); 
+    % Identifying bounding box for each well (rmin:rmax, cmin:cmax)
+    cmin = ceil(bb(i,1)); 
+    cmax = cmin + bb(i,3) - 1;
+    rmin = ceil(bb(i,2));
+    rmax = rmin + bb(i,4) - 1;    
+    img_here = regs(i).Image; %img_area_log(rowmin:rowmax, colmin:colmax);
     
     % Creating image for bulbs with the well number
-    img_temp = zeros(bb(i,4)+2, bb(i,3)+2);
-    img_temp_size = size(img_temp);
-    count = 0; 
-    for j = 0.85:0.1:1
+    img_temp = zeros(bb(i,4), bb(i,3));
+    img_temp_size = size(img_temp); count_bulb = 0; 
+    for j = 0.85:0.01:1
         [cen, rad] = imfindcircles(img_here, [37 47], 'Sensitivity', j);
         if isempty(rad)
-            count = count + 1; 
-            continue; 
+            count_bulb = count_bulb + 1; 
+            continue;
         elseif (length(rad)>1)
-            fprintf('Found multiple bulbs. Pause and Debug iteration %d \n',i);
+            fprintf('Found multiple bulbs in iteration %d. Selecting the right most bulb and moving on. \n',i);
+%             figure
 %             imshow(img_here);
 %             viscircles(cen, rad);
+%             % Circles that reach outside the BB are removed
+%             cen_rmin = cen(:,2)-rad; idx_rmin = find(cen_rmin<0);
+%             cen_rmax = cen(:,2)+rad; idx_rmax = find(cen_rmax>bb(i,4)); 
+%             cen_cmin = cen(:,1)-rad; idx_cmin = find(cen_cmin<0);
+%             cen_cmax = cen(:,1)+rad; idx_cmax = find(cen_cmax>bb(i,3)); 
+%             idx = [idx_rmin; idx_rmax; idx_cmin; idx_cmax]; 
+%             cen(idx,:) = []; rad(idx) = []; 
+%             figure
+%             imshow(img_here);
+%             viscircles(cen, rad);     
         end
         [~, pos] = max(cen(:,1)); % Identifying the farthest circle as bulb
         cen = cen(pos,:); rad = rad(pos); % Removing all other bulbs
-        bulb_colmin = floor(cen(1)-rad);
-        bulb_rowmin = floor(cen(2)-rad);
-        bulb_colmax = min(img_temp_size(2), ceil(cen(1)+rad));
-        bulb_rowmax = min(img_temp_size(1), ceil(cen(2)+rad)); 
-        for row = bulb_rowmin:bulb_rowmax
-            for col = bulb_colmin:bulb_colmax
-                if norm([row col]-cen)<rad
+        bulb_rmin = floor(cen(2)-rad);
+        bulb_cmin = floor(cen(1)-rad);
+        bulb_rmax = min(img_temp_size(1), ceil(cen(2)+rad)); 
+        bulb_cmax = min(img_temp_size(2), ceil(cen(1)+rad));
+        for row = bulb_rmin:bulb_rmax
+            for col = bulb_cmin:bulb_cmax
+                if norm([col row]-cen)<rad
                     img_temp(row,col) = i;
                 end
             end
         end
-%         viscircles(cen, rad);
         break;
     end
-    if (count == 16)
-        fprintf('Found no bulbs. Pause and Debug iteration %d \n',i);
+    if (count_bulb == 16)
+        fprintf('Found no bulbs in iteration %d. This well will be discarded. If you plan to debug, note that the order will change. \n',i);
+        count = count + 1; invalid_wells(count) = i; 
+    else
+        img_bulbs(rmin:rmax, cmin:cmax) = img_temp; 
+%         figure
+%         imshow(img_temp);
+        % Creating image for wells with the well number
+        img_temp = zeros(bb(i,4), bb(i,3));
+        img_temp(img_here == 1) = i;
+        img_wells(rmin:rmax, cmin:cmax) = img_temp;   
+%         figure
+%         imshow(img_temp);
     end
-    img_bulbs(rowmin:rowmax, colmin:colmax) = img_temp;            
-    
-% rectangle('Position',[bb(i,1) bb(i,2) bb(i,3) bb(i,4)],'EdgeColor','green');
+%     close all
+%     rectangle('Position',[bb(i,1) bb(i,2) bb(i,3) bb(i,4)],'EdgeColor','green');
 end
 
+invalid_wells(invalid_wells == 0) = []; 
+wells_list(invalid_wells,:) = []; 
 disp ('Image encoding done...');      
 
 %% 5. Writing to Excel
 write_name = 'wells_list.xlsx';
-wells_list = [cat(1,regs.Area) cat(1,regs.Centroid) cat(1,regs.BoundingBox)];
 writematrix(wells_list, write_name,'WriteMode','overwritesheet');
 
 %% 6. Drawing Figures for analysis
@@ -172,6 +188,7 @@ imshow(img_orgl(7000:8500, 7000:8500));
 % imshow(img_orgl);
 hold on
 visboundaries(img_area_log(7000:8500, 7000:8500),'color','r');
+visboundaries(img_bulbs(7000:8500, 7000:8500),'color','b');
 % visboundaries(img_area_log,'color','r');
 % savefig('Figure_wells_list'); 
 
